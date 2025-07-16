31d11707f437e14b1c2a5f45b24cd4aa22d753d4
Teirinvi <teirinvi@yandex.ru>
2025-07-16 19:51:38 +0300

    @ддобавит функции считываÑния и получения ввода пользователя

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,9 +2,33 @@ package main
 
 import "fmt"
 
+const currencyRateUsdEur = 0.85
+const currencyRateUsdRub = 77.9
+
 func main() {
-	const currencyRateUsdEur = 0.85
-	const currencyRateUsdRub = 77.9
 	currencyRateEurRub := currencyRateUsdRub / currencyRateUsdEur
-	fmt.Print(currencyRateEurRub)
+	fmt.Println(currencyRateEurRub)
+	userValue, userCurrencyInput, userCurrencyOutput := getUserInput()
+	getCurrencyValue(userValue, userCurrencyInput, userCurrencyOutput)
+}
+
+func getUserInput() (float64, string, string) {
+	var userValue float64
+	var userCurrencyInput string
+	var userCurrencyOutput string
+
+	fmt.Println("Введите количество валюты: ")
+	fmt.Scan(&userValue)
+	fmt.Println("Введите исходную валюту: ")
+	fmt.Scan(&userCurrencyInput)
+	fmt.Println("Введите целевую валюту: ")
+	fmt.Scan(&userCurrencyOutput)
+
+	return userValue, userCurrencyInput, userCurrencyOutput
+}
+
+func getCurrencyValue(userValue float64, userInput string, userOutput string) {
+	fmt.Println(userValue)
+	fmt.Println(userInput)
+	fmt.Println(userOutput)
 }
